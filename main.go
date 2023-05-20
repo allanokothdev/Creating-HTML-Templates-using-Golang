310072302315c613f0a269303fbcf85a6ee1e824
Allan Okoth <allanokoth695@gmail.com>
2023-05-21 01:58:27 +0200

    Update main.go

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Todo struct {
+	Title string
+	Done bool
+}
+
+type TodoPageData struct {
+	pageTitle string
+	Todos	[]Todo
+}
+
+func main() {
+	tmp1 := template.Must(template.ParseFiles("layout.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request)  {
+		data := TodoPageData{
+			pageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmp1.Execute(w, data)
+	})
+	http.ListenAndServe(":80", nil)
+}
